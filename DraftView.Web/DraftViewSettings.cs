47453b053f54e31fe7d42ab85bdf0a65681d96ef
AClarke6 <alastair_clarke@yahoo.com>
2026-04-08 14:25:39 +0100

    Add appsettings.Development.json to .gitignore

--- DraftView.Web/DraftViewSettings.cs
+++ DraftView.Web/DraftViewSettings.cs
@@ -33,6 +33,6 @@ public class SmtpSettings
     public int Port { get; set; } = 587;
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public string FromAddress { get; set; } = string.Empty;
+    public string From { get; set; } = string.Empty;
     public string FromName { get; set; } = "DraftView";
 }
