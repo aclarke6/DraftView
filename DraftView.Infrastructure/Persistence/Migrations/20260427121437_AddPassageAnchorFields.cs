3dc9bf14c52f75b96a1904e307976b973d4f877a
AClarke6 <alastair_clarke@yahoo.com>
2026-04-27 13:30:41 +0100

    Add passage anchor fields migration

---
+++ DraftView.Infrastructure/Persistence/Migrations/20260427121437_AddPassageAnchorFields.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DraftView.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPassageAnchorFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
